75bed3bbf60149817a8124ec5bf358835bae3bd1
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-03 17:05:09 +0530

    program can read commands

--- main.go
+++ main.go
@@ -1,49 +1,35 @@
 package main
 
 import (
-	"io"
-	"log"
+	"fmt"
 	"net"
-	"os"
 )
 
-type redisLog struct {
-	logError *log.Logger
-	logInfo  *log.Logger
-}
-
 func main() {
-	logError := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	logInfo := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
-	logInfo.Printf("Starting server on port :6379")
+	logs := NewLogger()
+	logs.logInfo.Printf("Starting server on port :6379")
 
 	// create a new server
 	lsn, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		logError.Fatal(err)
+		logs.ServerError(err)
 		return
 	}
 	// listen for connections
 	con, err := lsn.Accept()
 	if err != nil {
-		logError.Fatal(err)
+		logs.ServerError(err)
 		return
-	}
-	_ = redisLog{
-		logError: logError,
-		logInfo:  logInfo,
 	}
 	defer func(con net.Conn) { _ = con.Close() }(con)
 	for {
-		buf := make([]byte, 1024)
-		_, err := con.Read(buf)
+		resp := NewResp(con)
+		value, err := resp.Read()
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			logError.Fatal(err)
+			logs.ServerError(err)
+			return
 		}
+		fmt.Println(value)
 		// ignore request and send back a PONG
 		_, _ = con.Write([]byte("+OK\r\n"))
 	}
