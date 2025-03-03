d52698fc9828deab4bc2b17dc1e7659ca578e39a
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-03 15:34:17 +0530

    starts a server for redis client

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	logError := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	logInfo := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	logInfo.Printf("Starting server on port :6379")
+
+	// create a new server
+	lsn, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		logError.Fatal(err)
+		return
+	}
+	// listen for connections
+	con, err := lsn.Accept()
+	if err != nil {
+		logError.Fatal(err)
+		return
+	}
+	defer func(con net.Conn) { _ = con.Close() }(con)
+	for {
+		buf := make([]byte, 1024)
+
+		// read message from client
+		_, err := con.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			logInfo.Println("Error reading from client: ", err)
+			os.Exit(1)
+		}
+		// ignore request and send back a PONG
+		_, _ = con.Write([]byte("+OK\r\n"))
+	}
 }
