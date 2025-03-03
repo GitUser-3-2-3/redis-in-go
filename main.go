08362b68564cbaac92722af97ae7715690914e95
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-03 17:38:44 +0530

    removed unnecessary code

--- main.go
+++ main.go
@@ -1,35 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
+	"os"
 )
 
 func main() {
-	logs := NewLogger()
-	logs.logInfo.Printf("Starting server on port :6379")
+	logError := log.New(os.Stderr, "ERROR::\t", log.Ldate|log.Ltime|log.Lshortfile)
+	logInfo := log.New(os.Stdout, "INFO::\t", log.Ldate|log.Ltime)
+
+	logInfo.Printf("Starting server on port :6379")
 
 	// create a new server
 	lsn, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		logs.ServerError(err)
+		logError.Fatal(err)
 		return
 	}
 	// listen for connections
 	con, err := lsn.Accept()
 	if err != nil {
-		logs.ServerError(err)
+		logError.Fatal(err)
 		return
 	}
 	defer func(con net.Conn) { _ = con.Close() }(con)
 	for {
-		resp := NewResp(con)
-		value, err := resp.Read()
+		rsp := &resp{reader: bufio.NewReader(con)}
+		val, err := rsp.Read()
 		if err != nil {
-			logs.ServerError(err)
+			logError.Fatal(err)
 			return
 		}
-		fmt.Println(value)
+		fmt.Println(val)
 		// ignore request and send back a PONG
 		_, _ = con.Write([]byte("+OK\r\n"))
 	}
--- resp.go
+++ resp.go
@@ -3,11 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"strconv"
 )
-
-// todo -> better error handling
 
 const (
 	STRING  = '+'
@@ -17,25 +14,21 @@ const (
 	ARRAY   = '*'
 )
 
-type Value struct {
+type value struct {
 	typ   string
 	str   string
 	num   int
 	bulk  string
-	array []Value
+	array []value
 }
 
-type Resp struct {
+type resp struct {
 	reader *bufio.Reader
 }
 
-func NewResp(rd io.Reader) *Resp {
-	return &Resp{bufio.NewReader(rd)}
-}
-
-func (resp *Resp) readLine() (line []byte, bytlen int, err error) {
+func (rsp *resp) readLine() (line []byte, bytlen int, err error) {
 	for {
-		byt, err := resp.reader.ReadByte()
+		byt, err := rsp.reader.ReadByte()
 		if err != nil {
 			return nil, 0, err
 		}
@@ -48,8 +41,8 @@ func (resp *Resp) readLine() (line []byte, bytlen int, err error) {
 	return line[:len(line)-2], bytlen, nil
 }
 
-func (resp *Resp) readInteger() (x, bytlen int, err error) {
-	line, bytlen, err := resp.readLine()
+func (rsp *resp) readInteger() (x, bytlen int, err error) {
+	line, bytlen, err := rsp.readLine()
 	if err != nil {
 		return 0, 0, err
 	}
@@ -60,34 +53,34 @@ func (resp *Resp) readInteger() (x, bytlen int, err error) {
 	return int(i64), bytlen, nil
 }
 
-func (resp *Resp) Read() (Value, error) {
-	bytTyp, err := resp.reader.ReadByte()
+func (rsp *resp) Read() (value, error) {
+	bytTyp, err := rsp.reader.ReadByte()
 	if err != nil {
-		return Value{}, err
+		return value{}, err
 	}
 	switch bytTyp {
 	case ARRAY:
-		return resp.readArray()
+		return rsp.readArray()
 	case BULK:
-		return resp.readBulk()
+		return rsp.readBulk()
 	default:
 		fmt.Printf("Unknown type: %v", string(bytTyp))
-		return Value{}, nil
+		return value{}, nil
 	}
 }
 
-func (resp *Resp) readArray() (Value, error) {
-	val := Value{}
+func (rsp *resp) readArray() (value, error) {
+	val := value{}
 	val.typ = "array"
 
-	ln, _, err := resp.readInteger()
+	ln, _, err := rsp.readInteger()
 	if err != nil {
 		return val, err
 	}
-	val.array = make([]Value, ln)
+	val.array = make([]value, ln)
 
 	for i := 0; i < ln; i++ {
-		v, err := resp.Read()
+		v, err := rsp.Read()
 		if err != nil {
 			return val, err
 		}
@@ -96,21 +89,25 @@ func (resp *Resp) readArray() (Value, error) {
 	return val, nil
 }
 
-func (resp *Resp) readBulk() (Value, error) {
-	val := Value{}
+func (rsp *resp) readBulk() (value, error) {
+	val := value{}
 	val.typ = "bulk"
 
-	ln, _, err := resp.readInteger()
+	ln, _, err := rsp.readInteger()
 	if err != nil {
 		return val, err
 	}
 	bulk := make([]byte, ln)
-	_, _ = resp.reader.Read(bulk)
-
+	_, err = rsp.reader.Read(bulk)
+	if err != nil {
+		return value{}, err
+	}
 	val.bulk = string(bulk)
 
 	// read the trailing CRLF
-	_, _, _ = resp.readLine()
-
+	_, _, err = rsp.readLine()
+	if err != nil {
+		return value{}, err
+	}
 	return val, nil
 }
